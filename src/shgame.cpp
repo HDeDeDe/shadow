64779b7d127c31fae89ced6bfb30107ad2b92855
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-06-19 15:07:39 -0700

    changed defaults

--- src/shgame.cpp
+++ src/shgame.cpp
@@ -4,24 +4,31 @@
 
 // ---------- Shadow ----------
 
-void sh::play::ShadowStart()
-{
 
-    sh::auditorium::texture::sh_TextureManager::Create("super_vergil", "./resource/supe.png");
-    std::cout << GetWorkingDirectory() << std::endl;
-    sh::auditorium::texture::sh_TextureManager::Load("super_vergil");
-    sh::auditorium::texture::sh_Texture *sh_cached = sh::auditorium::texture::sh_TextureManager::GetTexturePointer("super_vergil");
-    while (sh::gameLoopActive)
-    {
-        if (WindowShouldClose())
+bool shExit(bool killApp = false)
+{
+    if (WindowShouldClose() || killApp)
         {
             sh::gameLoopActive = false;
             sh::auditorium::closeWindow();
-            break;
+            return true;
         }
+    return false;
+}
+
+void sh::play::ShadowStart()
+{
+    //Create lua state and open libraries
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    
+    while (sh::gameLoopActive)
+    {
+        if(shExit())break;
+
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawTexture(sh_cached->getTexture(), 0, 0, WHITE);
+        
         DrawFPS(0, 0);
         EndDrawing();
     }
