6486e208dd14254e5fe337b77b87b388c9aa3700
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-06-24 14:55:22 -0700

    Camera changes

--- src/shCamera.cpp
+++ src/shCamera.cpp
@@ -3,6 +3,8 @@
 sh::auditorium::viewport::sh_camera::sh_camera(int fov)
 {
     m_dimension3D.Size((float)(fov));
+    m_3DCam.projection = CAMERA_PERSPECTIVE;
+    SetCameraMode(m_3DCam, CAMERA_CUSTOM);
 }
 
 void sh::auditorium::viewport::sh_camera::setOrigin()
@@ -15,4 +17,8 @@ void sh::auditorium::viewport::sh_camera::updateCameras()
     m_2DCam.rotation = m_dimension2D.Roll;
     m_2DCam.zoom = m_dimension2D.Size();
     m_2DCam.target = Vector2{m_dimension2D.X, m_dimension2D.Y};
+
+    m_3DCam.fovy = m_dimension3D.Size();
+    m_3DCam.position = Vector3{m_dimension3D.X, m_dimension3D.Y, m_dimension3D.Z};
+    //TODO: Figure out how to convert the target system to a yaw-pitch-roll system.
 }
