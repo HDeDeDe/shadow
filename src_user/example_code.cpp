cc9c20e7417c1722e2c05a21884c2e6e1481606f
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-06-26 14:58:58 -0700

    model loading hopefully

--- src_user/example_code.cpp
+++ src_user/example_code.cpp
@@ -8,6 +8,9 @@
 
 std::string ExampleText;
 sh::Dimension ExampleDimension;
+
+
+
 // ---------- Shadow ----------
 
 void sh::play::GameInit() //This is where you initialize any nesecary code
@@ -29,6 +32,7 @@ void sh::play::GameLoop() //This is where the main game loop occurrs, rendering 
 {
     sh::auditorium::draw::queueHUD(DTEXTURE, "Test");
     sh::auditorium::draw::queueHUD(DTEXT, ExampleText, ExampleDimension, RED);
+    sh::auditorium::viewport::GlobalCamera.updateCameras();
 }
 
 //This is where you put any code relating to Dear imgui
@@ -42,14 +46,39 @@ void sh::play::ImguiDebugDraw()
     ImGui::Checkbox("Render 2D", &is2D);
     ImGui::Checkbox("Render 3D", &is3D);
     ImGui::Checkbox("Render HUD", &isHUD);
+
     float dbg_colors[4] = {((float)sh::auditorium::draw::bgDefault.r / 255.0f), ((float)sh::auditorium::draw::bgDefault.g  / 255.0f), ((float)sh::auditorium::draw::bgDefault.b / 255.0f), ((float)sh::auditorium::draw::bgDefault.a / 255.0f)};
     ImGui::ColorEdit4("Background Color", dbg_colors);
     sh::auditorium::draw::bgDefault = CLITERAL(Color){(unsigned char)(dbg_colors[0] * 255.0f),(unsigned char)(dbg_colors[1] * 255.0f),(unsigned char)(dbg_colors[2]* 255.0f),(unsigned char)255};
-    sh::auditorium::viewport::sh_camera dbg_globalCam = sh::auditorium::viewport::GlobalCamera;
-    float dbg_3dCamPos[3] = {dbg_globalCam.getCamera3D().position.x, dbg_globalCam.getCamera3D().position.y, dbg_globalCam.getCamera3D().position.z};
+    
+    sh::Dimension temp_cam2D =  sh::auditorium::viewport::GlobalCamera.getDimension2D();
+    sh::Dimension temp_cam3D =  sh::auditorium::viewport::GlobalCamera.getDimension3D();
+    Vector3 temp_3DTarg = sh::auditorium::viewport::GlobalCamera.get3DTarget();
+
+    float dbg_3dCamPos[3] = {temp_cam3D.X, temp_cam3D.Y, temp_cam3D.Z};
     ImGui::DragFloat3("Camera3D Position", dbg_3dCamPos);
-    float dbg_2dCamPos[2] = {dbg_globalCam.getCamera2D().target.x, dbg_globalCam.getCamera2D().target.y};
+    ImGui::Text("Camera3D FOV: %d", sh::auditorium::viewport::GlobalCamera.getCamera3D().fovy);
+
+    float dbg_3dCamTarg[3] = {temp_3DTarg.x, temp_3DTarg.y, temp_3DTarg.z};
+    ImGui::DragFloat3("Camera3D Target", dbg_3dCamTarg);
+
+    float dbg_2dCamPos[2] = {temp_cam2D.X, temp_cam2D.Y};
     ImGui::DragFloat2("Camera2D Position", dbg_2dCamPos);
-    sh::auditorium::viewport::GlobalCamera.setCamPos(Vector3{dbg_3dCamPos[0], dbg_3dCamPos[1], dbg_3dCamPos[2]}, Vector2{dbg_2dCamPos[0], dbg_2dCamPos[1]});
+    ImGui::Text("Camera2D Zoom: %d", sh::auditorium::viewport::GlobalCamera.getCamera2D().zoom);
+
+    temp_cam2D.X = dbg_2dCamPos[0];
+    temp_cam2D.Y = dbg_2dCamPos[1];
+    
+    temp_cam3D.X = dbg_3dCamPos[0];
+    temp_cam3D.Y = dbg_3dCamPos[1];
+    temp_cam3D.Z = dbg_3dCamPos[2];
+
+    temp_3DTarg.x = dbg_3dCamTarg[0];
+    temp_3DTarg.y = dbg_3dCamTarg[1];
+    temp_3DTarg.z = dbg_3dCamTarg[2];
+
+    sh::auditorium::viewport::GlobalCamera.setDimension3D(temp_cam3D);
+    sh::auditorium::viewport::GlobalCamera.setDimension2D(temp_cam2D);
+    sh::auditorium::viewport::GlobalCamera.set3DTarget(temp_3DTarg);
 }
 #endif
