4f96e7d368881e56fc0df873eaa21f1d1b11bb4f
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-06-28 20:54:45 -0700

    builtin imgui change

--- src_dear_imgui/shImguiBuiltIn.cpp
+++ src_dear_imgui/shImguiBuiltIn.cpp
@@ -55,15 +55,4 @@ void sh::debug::DebugImgui()
     sh::auditorium::viewport::GlobalCamera.setDimension3D(temp_cam3D);
     sh::auditorium::viewport::GlobalCamera.setDimension2D(temp_cam2D);
     // sh::auditorium::viewport::GlobalCamera.set3DTarget(temp_3DTarg);
-    if(ImGui::Button("Toggle Fullscreen"))
-    {
-        if(sh::auditorium::isFullscreen())
-        {
-            sh::auditorium::setFullscreen(false);
-        }
-        else
-        {
-            sh::auditorium::setFullscreen(true);
-        }
-    }
 }
