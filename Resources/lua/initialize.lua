7ae4a7f72fc6ffaa853fba1b73e7e0c601948175
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-06-30 19:55:10 -0700

    comment changes

--- Resources/lua/initialize.lua
+++ Resources/lua/initialize.lua
@@ -1,4 +1,5 @@
 --This file is called when the engine is started. Anything defined here can be called with GetGlobalLua()
+--Be sure to put your moduels in lua/packages and invoke them with: local myPackage = require('lua/packages/myPackage')
 TextToDisplay = "Hello World!"
 PictureToLoad = "./Test.png"
 ModelToLoad = "./Test.iqm"
