15e5e8a1c9806bb3646ef2a4d9f57a3e0a76e82d
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-07-03 20:13:56 -0700

    added inifile to initialize.lua

--- Resources/lua/initialize.lua
+++ Resources/lua/initialize.lua
@@ -1,5 +1,7 @@
 --This file is called when the engine is started. Anything defined here can be called with GetGlobalLua()
 --Be sure to put your moduels in lua/packages and invoke them with: local myPackage = require('lua/packages/myPackage')
+local inifile = require('lua/packages/inifile')
+--This is recomended so that you can access INI saving
 TextToDisplay = "Hello World!"
 PictureToLoad = "./Test.png"
 ModelToLoad = "./Test.iqm"
