7d4b537eae443efd1b2ea8dbef51340570072d2e
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-07-03 19:43:47 -0700

    Added input detection

--- src/shgame.cpp
+++ src/shgame.cpp
@@ -2,6 +2,7 @@
 #include <shadow.hpp>
 #include <shAuditorium.hpp>
 #include <shLua.hpp>
+#include <math.h>
 // ---------- Shadow ----------
 
 void sh::play::ShadowStart()
@@ -15,4 +16,91 @@ void sh::play::ShadowStart()
         sh::auditorium::draw::drawScreen(sh::auditorium::viewport::GlobalCamera);
     }
     sh::lua::CloseLuaGlobal();
+}
+
+
+
+bool sh::input::checkInput(KeyboardKey key, inputType input)
+{
+    switch(input)
+    {
+        case INPUT_DOWN:
+        {
+            return IsKeyDown(key);
+        }
+        case INPUT_PRESSED:
+        {
+            return IsKeyPressed(key);
+        }
+        case INPUT_RELEASE:
+        {
+            return IsKeyReleased(key);
+        }
+        case INPUT_UP:
+        {
+            return IsKeyUp(key);
+        }
+        default: return false;
+    }
+}
+
+bool sh::input::checkInput(MouseButton key, inputType input)
+{
+    switch(input)
+    {
+        case INPUT_DOWN:
+        {
+            return IsMouseButtonDown(key);
+        }
+        case INPUT_PRESSED:
+        {
+            return IsMouseButtonPressed(key);
+        }
+        case INPUT_RELEASE:
+        {
+            return IsMouseButtonReleased(key);
+        }
+        case INPUT_UP:
+        {
+            return IsMouseButtonUp(key);
+        }
+        default: return false;
+    }
+}
+
+bool sh::input::checkInput(GamepadButton key, int gamepad, inputType input)
+{
+    if(IsGamepadAvailable(gamepad))
+    {
+        switch(input)
+        {
+            case INPUT_DOWN:
+            {
+                return IsGamepadButtonDown(gamepad, key);
+            }
+            case INPUT_PRESSED:
+            {
+                return IsGamepadButtonPressed(gamepad, key);
+            }
+            case INPUT_RELEASE:
+            {
+                return IsGamepadButtonReleased(gamepad, key);
+            }
+            case INPUT_UP:
+            {
+                return IsGamepadButtonUp(gamepad, key);
+            }
+            default: return false;
+        }
+    }
+    return false;
+}
+
+float sh::input::checkInput(GamepadAxis key, int gamepad)
+{
+    if(IsGamepadAvailable(gamepad))
+    {
+        return GetGamepadAxisMovement(gamepad, key);
+    }
+    return NAN;
 }
