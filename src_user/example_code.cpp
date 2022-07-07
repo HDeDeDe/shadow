c40093e7ef36a3a4ef2a5b4eed32834340a3cf17
HDeDeDe <21134925+HDeDeDe@users.noreply.github.com>
2022-07-07 14:54:58 -0700

    Updated input check

--- src_user/example_code.cpp
+++ src_user/example_code.cpp
@@ -51,7 +51,7 @@ void sh::play::GameInit() //This is where you initialize any nesecary code
 
 void sh::play::GameLoop() //This is where the main game loop occurrs, rendering is handled outside of this loop
 {
-    if(sh::input::checkInput(sh::input::MOUSE_1, sh::input::INPUT_DOWN)) ExampleInputPressed = true;
+    if(sh::input::checkInput(ExampleInput, sh::input::INPUT_DOWN)) ExampleInputPressed = true;
     for(int i = 0; (i < frameDiff) && ExampleInputPressed; i++)
     {
         spaget = GetMouseRay(GetMousePosition(), sh::auditorium::viewport::GlobalCamera.getCamera3D());
